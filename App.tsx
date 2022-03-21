6b22d6dd704b73f0f65a3d4e59b32d4241f99851
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-21 22:29:53 +0900

    [機能追加]ログイン機能追加

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import "react-native-gesture-handler";
 import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { ChatScreen } from "./src/screens/ChatScreen";
+import { auth } from "./firebase";
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // ログイン中
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+    return unsubscribe;
+  });
 
   return (
-    ChatScreen()
+    <NavigationContainer>
+      {loggedIn ? <ChatScreen /> : <ChatScreen />}
+    </NavigationContainer>
   );
 }
