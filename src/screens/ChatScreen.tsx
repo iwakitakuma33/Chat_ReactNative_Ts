60891e595688bb5d83262e53ddd63218028edf41
iwakitakuma33 <iwakitakuma33@gmail.com>
2022-03-22 23:51:11 +0900

    [機能追加]チャット画面実装

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,16 +1,93 @@
 // src/screens/ChatScreen.js
 import * as React from "react";
+import {Platform} from 'react-native';
 import {
   SafeAreaView,
   View,
+  StyleSheet,
+  KeyboardAvoidingView,
 } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import { useNavigation } from "@react-navigation/core";
+import { Button } from "react-native-paper";
+import { auth, db } from "../../firebase";
 
+type Message = {
+  _id: any;
+  createdAt: any; 
+  text: any; 
+  user: any;
+}
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
 
 export const ChatScreen = () => {
+  const navigation = useNavigation();
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        console.log("Sign out success!");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button onPress={signOut}>ログアウト</Button>,
+    });
+  }, []);
+
+  // 表示するチャットメッセージの配列
+  const [messages, setMessages] = React.useState<Message[]>([]);
+
+  React.useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            _id: doc.data()._id,
+            createdAt: doc.data().createdAt.toDate(),
+            text: doc.data().text,
+            user: doc.data().user,
+          }))
+        )
+      );
+    return unsubscribe;
+  }, []);
+
+  const onSend = React.useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    const { _id, createdAt, text, user } = messages[0];
+    db.collection("chats").add({
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
+
   return (
-    <SafeAreaView>
-      <View>
-        hello
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <GiftedChat
+          messages={messages}
+          placeholder="メッセージを入力してください..."
+          onSend={onSend}
+          user={{
+            _id: auth?.currentUser?.email ? auth?.currentUser.email : Math.round(Math.random() * 100000000),
+          }}
+        />
+        {Platform.OS === "android" && (
+          <KeyboardAvoidingView behavior="padding" />
+        )}
       </View>
     </SafeAreaView>
   );
